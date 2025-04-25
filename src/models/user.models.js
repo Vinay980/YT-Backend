918ce6ac1fe1f20e4b1d4747657d444367eebaed
vinay980 <vinaypatel980pp@gmail.com>
2025-04-26 01:00:50 +0530

    adding model part

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -2,8 +2,48 @@ import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema(
     {
-
-    }
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            lowercase: true,
+            required: true,
+            trim: true,
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String, //cloudinary URL
+        },
+        coverImage: {
+            type: String, //cloudinary URL
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, "Password is required"]
+        },
+        refreshToken: {
+            type: String
+        }
+    },
+    { timestamps: true }
 )
 
 export const User = mongoose.model("User", userSchema)
